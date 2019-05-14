9c773622a138d30993f82f905c5a0fd8fbe7fcb5
Shivam Vats <shivamvats.iitkgp@gmail.com>
2019-05-14 16:01:04 -0400

    Added functions to planning space and robot_state.

--- src/space/lattice_planning_space.cpp
+++ src/space/lattice_planning_space.cpp
@@ -1,7 +1,10 @@
+#include <cmath.h>
+
 #include "hsearch/space/lattice_planning_space.h"
 
 namespace hsearch {
     LatticePlanningSpace::LatticePlanningSpace(
+            OccupancyGridPtr &occ_grid_ptr_,
             ActionSpacePtr &action_space_ptr_,
             RobotState start_ ) :
         // collision_checker,
@@ -16,7 +19,41 @@ namespace hsearch {
         return succs;
     }
 
+    bool LatticePlanningSpace::setStart( const RobotState state_ ){
+        //if( isValid( start_ ) ){
+        //    m_start = start_;
+        //    return true;
+        //}
+        //else{
+        //    ROS_ERROR("Start location is invalid.");
+        //    return false;
+        //}
+        m_start = state_;
+        return true;
+    }
+
+    bool LatticePlanningSpace::setGoal( const RobotState& state_ ){
+        m_goal = state_;
+        return true;
+    }
+
+    bool LatticePlanningSpace::setGoalThresh( double thresh_ ){
+        m_goal_thresh = thresh_;
+    }
+
+    bool LatticePlanningSpace::isGoal( const RobotState& state_ ) const {
+        for( size_t i=0; i<dim(); i++ ){
+            if( fabs( state_[i] - m_goal[i] ) > m_goal_thresh )
+                return false;
+        }
+        return true;
+    }
+
     size_t LatticePlanningSpace::dim() const {
         return m_action_space_ptr->dim();
+    }
+
+    NodeId LatticePlanningSpace::robotStateToNodeId( RobotState& robot_state_ ){
+
     }
 }
