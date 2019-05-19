f6bcc8707272e6ffd7cb8f5d567a5edf7d2a47c2
Shivam Vats <shivamvats.iitkgp@gmail.com>
2019-05-19 19:13:49 -0400

    Added separate test file to test dijkstra.

--- test/dijkstra.cpp
+++ test/dijkstra.cpp
@@ -1,31 +1,12 @@
 #include <iostream>
 
 #include <hsearch/types.h>
-#include <hsearch/graph/directed_acyclic_graph.h>
-#include <hsearch/graph/lattice.h>
 #include <hsearch/space/action_space.h>
 #include <hsearch/space/lattice_planning_space.h>
+#include <hsearch/search/dijkstra.h>
 
 using namespace std;
 using namespace hsearch;
-
-void testDirectedAcyclicGraph(){
-    cout<<"Testing DirectedAcyclicGraph\n";
-    cout<<"============================\n";
-    DirectedAcyclicGraph graph( 1 );
-    graph.insertEdge( 1, 2 );
-    graph.insertEdge( 2, 3 );
-    graph.insertEdge( 1, 3 );
-
-    cout<<"Number of vertices: "<<graph.m_vertices_map.size()<<"\n";
-
-    auto succs = graph.Succs( 1 );
-    cout<<"Successors of Node 1\n";
-    for( auto el: succs ){
-        cout<<el<<"\t";
-    }
-    cout<<"\n\n";
-}
 
 OccupancyGrid createOccupancyGrid(){
     double size_x=10, size_y=10, size_z=2;
@@ -36,9 +17,9 @@ OccupancyGrid createOccupancyGrid(){
             res, origin_x, origin_y, origin_z, max_dist );
 }
 
-void testLattice(){
-    cout<<"Testing LatticePlanningSpace\n";
-    cout<<"============================\n";
+void testDijkstra(){
+    cout<<"Testing Dijkstra Planner\n";
+    cout<<"=========================\n";
     auto lattice_ptr = make_shared<Lattice>();
 
     auto grid = std::make_shared<OccupancyGrid>( createOccupancyGrid() );
@@ -52,9 +33,7 @@ void testLattice(){
     action_space_ptr->addAction( action3 );
     action_space_ptr->addAction( action4 );
 
-    RobotState start = { 0, 0, 0 };
-    cout<<"Start: \n";
-    printVector( start );
+    RobotState start( std::vector<double>{ 0, 0, 0 } );
     double res = 0.05;
     auto pspace_ptr = make_shared<LatticePlanningSpace>(
             grid, action_space_ptr, start, res );
@@ -62,14 +41,22 @@ void testLattice(){
     pspace_ptr->setGoal( goal );
     pspace_ptr->setGoalThresh( 0.1 );
 
-    auto succs = pspace_ptr->Succs( start );
-    cout<<"Succs: \n";
-    for( auto a : succs )
-        printVector( a );
-    cout<<"\n\n";
+    cout<<"Start:\n";
+    printVector( start );
+    cout<<"Goal:\n";
+    printVector( goal );
+    Dijkstra planner( pspace_ptr );
+
+    NodeIds soltn;
+    auto solved = planner.plan( 5, soltn );
+    if( solved )
+        cout<<"Failed to plan.\n";
+    else {
+        cout<<"Planning succeeded.\n";
+    }
+
 }
 
 int main(){
-    testDirectedAcyclicGraph();
-    testLattice();
+    testDijkstra();
 }
