7c5e2866b2ffef739d958edf7c80f1b12577eee1
Shivam Vats <shivamvats.iitkgp@gmail.com>
2019-06-21 15:30:23 -0400

    Test code for the visualizer

---
+++ test/visualizer.cpp
@@ -0,0 +1,10 @@
+#include <hsearch/types.h>
+#include <hsearch/viz/visualizer.h>
+
+using namespace std;
+using namespace hsearch;
+
+int main(){
+    Visualizer viz( 100, 100 );
+    viz.imshow();
+}
