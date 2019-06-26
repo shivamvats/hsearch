d410f59ebedf89292e376ea5078821156a8ac20e
Shivam Vats <shivamvats.iitkgp@gmail.com>
2019-06-26 12:39:51 -0400

    Added constructor to TwoDimGridSpace to initialize from opencv image.

--- include/hsearch/space/two_dim_grid_space.h
+++ include/hsearch/space/two_dim_grid_space.h
@@ -1,6 +1,12 @@
 #ifndef TWO_DIM_GRID_SPACE
 #define TWO_DIM_GRID_SPACE
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include "lattice_planning_space.h"
+
 namespace hsearch {
 
     class TwoDimGridSpace : public LatticePlanningSpace {
@@ -11,9 +17,15 @@ namespace hsearch {
                 RobotState start_ );
         // Initialize from an opencv image
         TwoDimGridSpace(
-                cv::Mat& img_,
+                const cv::Mat& img_,
                 const int connectivity_,
+                const double pixel_res_,
                 const cv::Point& start_ );
+
+        // Helper function
+        OccupancyGridPtr constructOccGrid( const cv::Mat&, const double pixel_res_ );
+        ActionSpacePtr constructActionSpace( const double pixel_res_, const int connectivity_ );
+
 
         public:
         int m_connectivity;
--- src/space/two_dim_grid_space.cpp
+++ src/space/two_dim_grid_space.cpp
@@ -9,46 +9,57 @@ namespace hsearch {
         LatticePlanningSpace( occ_grid_ptr_, action_space_ptr_, start_ )
     {}
 
-    TwoDimGridSpace::TwoDimGridSpace( 
-            cv::Mat& img_,
-            const int connectivity_,
-            const int pixel_res_,
-            const cv::Point& start_ ):
-            m_connectivity( connectivity_ ){
-        // Occupancy Grid
+    OccupancyGridPtr TwoDimGridSpace::constructOccGrid(
+            const cv::Mat& img_,
+            const double pixel_res_ ){
         int size_x = pixel_res_*img_.cols;
         int size_y = pixel_res_*img_.rows;
         int size_z = pixel_res_;
         double max_dist = 1;
         double origin_x=0, origin_y=0, origin_z=0;
-        occ_grid_ptr = std::make_shared<OccupancyGrid>( size_x, size_y,
+        OccupancyGridPtr occ_grid_ptr = std::make_shared<OccupancyGrid>( size_x, size_y,
                        size_z, pixel_res_, origin_x, origin_y, origin_z, max_dist );
+        return occ_grid_ptr;
+    }
 
-        // Action Space
-        ActionSpacePtr action_space_ptr = make_shared<Action>( 2 );
+    ActionSpacePtr TwoDimGridSpace::constructActionSpace(
+            const double pixel_res_,
+            const int connectivity_ ){
+        ActionSpacePtr action_space_ptr = std::make_shared<ActionSpace>( 2 );
         Action action1{ pixel_res_, 0 };
         Action action2{ -pixel_res_, 0 };
         Action action3{ 0, pixel_res_ };
         Action action4{ 0, -pixel_res_ };
-        action_space_ptr->addAction( action_1 );
-        action_space_ptr->addAction( action_2 );
-        action_space_ptr->addAction( action_3 );
-        action_space_ptr->addAction( action_4 );
+        action_space_ptr->addAction( action1 );
+        action_space_ptr->addAction( action2 );
+        action_space_ptr->addAction( action3 );
+        action_space_ptr->addAction( action4 );
         if( m_connectivity == 8 ){
             Action action5{ pixel_res_, pixel_res_ };
             Action action6{ pixel_res_, -pixel_res_ };
             Action action7{ -pixel_res_, pixel_res_ };
             Action action8{ -pixel_res_, -pixel_res_ };
-            action_space_ptr->addAction( action_5 );
-            action_space_ptr->addAction( action_6 );
-            action_space_ptr->addAction( action_7 );
-            action_space_ptr->addAction( action_8 );
+            action_space_ptr->addAction( action5 );
+            action_space_ptr->addAction( action6 );
+            action_space_ptr->addAction( action7 );
+            action_space_ptr->addAction( action8 );
         }
         else if( m_connectivity != 4 )
             throw std::invalid_argument(" Received invalid value for the connectivity parameter.");
-
-        std::vector<double> robot_start = {pixel_res_*start_.x, pixel_res_*start_.y };
-        LatticePlanningSpace( occ_grid_ptr, action_space_ptr, robot_start );
+        return action_space_ptr;
     }
 
+    TwoDimGridSpace::TwoDimGridSpace( 
+            const cv::Mat& img_,
+            const int connectivity_,
+            const double pixel_res_,
+            const cv::Point& start_ ):
+        LatticePlanningSpace(
+                constructOccGrid( img_, pixel_res_ ),
+                constructActionSpace( pixel_res_, connectivity_ ),
+                std::vector<double> {pixel_res_*start_.x,
+                pixel_res_*start_.y } ),
+        m_connectivity( connectivity_ ){}
+
 }
+
